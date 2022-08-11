7d7875bd60b50410893a6573a33727fa7453aeb9
Renzo Daniel Lavado Rivas <renzo.lavado@pragma.com.co>
2022-08-10 20:46:53 -0500

    feat: comment class

--- src/main/java/com/example/demoapi/interceptor/InterceptorConfig.java
+++ src/main/java/com/example/demoapi/interceptor/InterceptorConfig.java
@@ -1,21 +1,21 @@
-package com.example.demoapi.interceptor;
-
-import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Lazy
-@Configuration
-@AllArgsConstructor
-public class InterceptorConfig implements WebMvcConfigurer {
-
-    private final InterceptorHandler interceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor);
-    }
-
-}
+//package com.example.demoapi.interceptor;
+//
+//import lombok.AllArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Lazy;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Lazy
+//@Configuration
+//@AllArgsConstructor
+//public class InterceptorConfig implements WebMvcConfigurer {
+//
+//    private final InterceptorHandler interceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(interceptor);
+//    }
+//
+//}
